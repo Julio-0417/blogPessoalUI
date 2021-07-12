89a3b81d80469a8fc2efd8f2933c33f2f92c9cfd
unknown <julio.s.silva0417@gmail.com>
2021-07-12 10:59:52 -0300

    Implementação Put e Delete de Postagens

--- src/app/service/postagem.service.ts
+++ src/app/service/postagem.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment.prod';
 import { Observable } from 'rxjs';
@@ -14,19 +14,33 @@ export class PostagemService {
    token = {
     headers: new HttpHeaders().set('Authorization', environment.token)
   }
-/**
-  refreshToken(){
+
+  refreshToken(){   //verificar necessidade
     this.token = {
       headers: new HttpHeaders().set('Authorization', environment.token)
     }
-  }*/
+  }
 
   getAllPostagens(): Observable<Postagem[]>{
     return this.http.get<Postagem[]>('https://blogpessoaljulio.herokuapp.com/postagens',this.token)
   }
 
+  getByIdPostagem(id: number): Observable<Postagem>{
+    return this.http.get<Postagem>(`https://blogpessoaljulio.herokuapp.com/postagens/${id}`, this.token)
+  }
+
   postPostagem(postagem: Postagem): Observable<Postagem>{
     return this.http.post<Postagem>('https://blogpessoaljulio.herokuapp.com/postagens/salvar', postagem, this.token)
+  }
+
+  putPostagem(postagem: Postagem): Observable<Postagem>{
+    return this.http.put<Postagem>('https://blogpessoaljulio.herokuapp.com/postagens', postagem, this.token)
+  }
+
+  deletePostagem(id: number) {
+    let params = new HttpParams()
+    .set ('id', id)
+    return this.http.delete(`https://blogpessoaljulio.herokuapp.com/postagens?${params}`, this.token)
   }
 
 }
